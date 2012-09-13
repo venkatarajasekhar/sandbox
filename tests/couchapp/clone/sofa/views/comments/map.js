c48f272c37f6c6b9e24f66039c9139271baed606
Emmanuel Mayssat <emmanuel.mayssat@aya.yale.edu>
2012-09-13 23:51:04 +0000

    With sofa

--- tests/couchapp/clone/sofa/views/comments/map.js
+++
@@ -1,7 +0,0 @@
-function(doc) {  
-  var comments = require("views/lib/comments");
-  
-  if (doc.type == "comment") {
-    emit(new Date(doc.created_at), comments.withGravatar(doc));
-  }
-};
--- tests/couchapp/clone/sofa/views/post-page/map.js
+++
@@ -1,9 +0,0 @@
-function(doc) {
-  var comments = require("views/lib/comments");
-  
-  if (doc.type == "post") {
-    emit([doc._id], doc);
-  } else if (doc.type == "comment") {
-    emit([doc.post_id, doc.created_at], comments.withGravatar(doc));
-  }
-};
