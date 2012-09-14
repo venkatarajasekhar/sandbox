7a4d9a51c69bb379ed900c5077c55c0e161cc1a8
Emmanuel.Mayssat <emayssat@gmail.com>
2012-09-14 22:55:15 +0000

    With first validation

---
+++ tests/couchdb/validation/validation_01.js
@@ -0,0 +1,19 @@
+function(newDoc, oldDoc, userCtx) {
+    function require(field, message) {
+        message = message || "Document mist have a " + field;
+        if (!newDoc[field]) throw({forbidden: message});
+    };
+
+    if (newDoc.type == "post") {
+        require("title");
+        require("created_at");
+        require("body");
+        require("author");
+    }
+
+    if (newDoc.type == "comment") {
+        require("name");
+        require("created_at");
+        require("comment", "You may not leave an empty comment");
+    }
+}
