0a5cac395639bb303ddc276e569169d32e6bbe8a
Emmanuel.Mayssat <emayssat@gmail.com>
2012-09-19 17:26:22 +0000

    With a working interesting stuff

--- tests/couchapp/apps/05_logbook/logbook/views/recent-items/map.js
+++ tests/couchapp/apps/05_logbook/logbook/views/recent-items/map.js
@@ -1,10 +1,29 @@
 function(doc) {
     if (doc.created_at) {
         var p = doc.profile || {};
+        var d = new Date(doc.created_at);
+        var day = d.getDate();
+        var dayOfWeek = d.getDay() + 1;
+        var month = d.getMonth() +1;
+        var year = d.getFullYear();
+        var hours = d.getHours();
+        var minutes = d.getMinutes();
+        var seconds = d.getSeconds();
+
+        if (minutes <10) { 
+            minutes = "0" + minutes;
+        }
+
+
+        var $time = hours + ":" + minutes + ":" + seconds;
+        var $date = year + "/" + month + "/" + day;
+
         emit(doc.created_at, {
             message:doc.message,
             gravatar_url : p.gravatar_url,
             nickname : p.nickname,
+            date : d.toLocaleDateString(),
+            time : d.toLocaleTimeString(),
             name : doc.name,
         });
     }
