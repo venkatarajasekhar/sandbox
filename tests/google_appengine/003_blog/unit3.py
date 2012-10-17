3d124057281390e68def185981a3285a229fff33
Emmanuel.Mayssat <emayssat@gmail.com>
2012-10-16 22:16:03 -0700

    With unit3 stuff working

--- tests/google_appengine/003_blog/unit3.py
+++ tests/google_appengine/003_blog/unit3.py
@@ -20,9 +20,31 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+class Art(db.Model):
+    title = db.StringProperty(required = True)
+    art = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
 class MainPage(Handler):
+
+    def render_front(self, title="", art="", error=""):
+        arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
+        self.render("front.html", title=title, art=art, error=error, arts = arts)
+
     def get(self):
-        self.write("asciichan!")
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        art = self.request.get("art")
+
+        if title and art:
+            a = Art(title = title, art = art)
+            a.put()
+            self.redirect("/")
+        else:
+            error = "we need both a title and some artwork!"
+            self.render_front(title, art, error)
 
 
 
