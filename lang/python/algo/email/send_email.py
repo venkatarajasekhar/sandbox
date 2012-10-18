e21ed4a9ab76756049fec768774089034c33fe9a
Emmanuel.Mayssat <emayssat@gmail.com>
2012-10-17 17:35:13 -0700

    With send_mail

---
+++ lang/python/algo/email/send_email.py
@@ -0,0 +1,17 @@
+import smtplib  
+      
+fromaddr = 'reciprocity.mail.noreply@gmail.com'  
+toaddrs  = 'emayssat@gmail.com'  
+msg = 'There was a terrible error that occured and I wanted you to know!'  
+      
+      
+# Credentials (if needed)  
+username = 'lynceantech@earthlink.net'  
+password = 'yasux72d'  
+      
+# The actual mail send  
+server = smtplib.SMTP('smtpauth.earthlink.net:587')  
+server.starttls()  
+server.login(username,password)  
+server.sendmail(fromaddr, toaddrs, msg)  
+server.quit()  
