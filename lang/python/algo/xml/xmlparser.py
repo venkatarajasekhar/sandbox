5b6f4e96e7001eb97217cdf7752f301d4d481a84
Emmanuel.Mayssat <emayssat@gmail.com>
2012-10-22 16:51:29 -0700

    With json

--- lang/python/algo/xml/xmlparser.py
+++ lang/python/algo/xml/xmlparser.py
@@ -3,6 +3,8 @@
 # minidom is simple and fast!
 from xml.dom import minidom            
 
+import urllib
+
 html = "<mytag>contents<children><item>1</item><item>2</item></children></mytag>"
 minidom.parseString(html)
 
@@ -15,3 +17,10 @@ print x.getElementsByTagName("item")
 print x.getElementsByTagName("item")[0]
 print x.getElementsByTagName("item")[0].childNodes[0]
 print x.getElementsByTagName("item")[0].childNodes[0].nodeValue
+
+p = urllib.urlopen("http://www.nytimes.com/services/xml/rss/nyt/GlobalHome.xml")
+c = p.read()
+x = minidom.parseString(c)
+print x.getElementsByTagName("item")
+print len(x.getElementsByTagName("item"))
+
