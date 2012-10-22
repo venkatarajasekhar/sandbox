e3ec11b4a3d176e89a4322f2295feaab29a081a6
Emmanuel.Mayssat <emayssat@gmail.com>
2012-10-22 15:34:40 -0700

    With rss in progress

--- lang/python/algo/xml/xmlparser.py
+++ lang/python/algo/xml/xmlparser.py
@@ -5,3 +5,13 @@ from xml.dom import minidom
 
 html = "<mytag>contents<children><item>1</item><item>2</item></children></mytag>"
 minidom.parseString(html)
+
+x = minidom.parseString(html)
+print dir(x)
+
+print x.toprettyxml()
+
+print x.getElementsByTagName("item")
+print x.getElementsByTagName("item")[0]
+print x.getElementsByTagName("item")[0].childNodes[0]
+print x.getElementsByTagName("item")[0].childNodes[0].nodeValue
