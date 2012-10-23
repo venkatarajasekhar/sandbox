85e5e1a8b1c541c269fe50cd1911f224cf01997d
Emmanuel.Mayssat <emayssat@gmail.com>
2012-10-22 17:30:24 -0700

    Iwth py2json

--- lang/python/algo/json/py2json.py
+++ lang/python/algo/json/py2json.py
@@ -4,3 +4,5 @@ import json
 
 print json.dumps([1,2,3])
 print json.dumps({"one":1, "two":2})
+print json.dumps({"one":1, "two": 'the main said"cool!"'})                  # <-- automatic escaping of quotes
+
