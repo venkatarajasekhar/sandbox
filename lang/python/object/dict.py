2f3fe3633938ee567e12208f6aa43c2f2b49f1d9
Emmanuel.Mayssat <emayssat@gmail.com>
2012-10-23 17:09:25 -0700

    With dict

--- lang/python/object/dict.py
+++ lang/python/object/dict.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+def section(s):
+    print "\n*** %s ***\n" % s.upper()
 
-
+section("INTRO")
 d1 = dict()
 d2 = {}
+
+print d1 == d2                                      # True
 
 
 d1['a']='AAAAA'
@@ -17,3 +21,38 @@ except NameError as e:
     print "NameError %s" % e
 except KeyError as e:
     print "KeyError %s" % e
+
+
+#----------------------------------------------------------------------
+section("KEYS")
+
+d = dict()
+t = (2,3)                                           # You can use tuple as dictionary keys !!!
+d[t] = "hello"
+print d[t]
+
+print t in d
+print (2,3) in d
+print (4,3) in d
+
+#----------------------------------------------------------------------
+section( "CACHING" )
+import time
+
+cache = dict()
+def complex_computation(a, b):
+    time.sleep(.5)
+    return a + b
+
+def cached_computation(a, b):
+    key = (a, b)
+    try:
+        return cache[key]                                       # <-- instantaneous 
+    except KeyError:
+        cache[key] = complex_computation(a,b)                   # <-- take .5 sec
+        return cache[key]
+
+print cached_computation(1,2)
+print cached_computation(1,2)
+print cached_computation(4,2)
+
