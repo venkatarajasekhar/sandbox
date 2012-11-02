0f24351a1b570add36e900e456a69820cfd716a0
Emmanuel.Mayssat <emayssat@gmail.com>
2012-11-02 13:45:43 -0700

    With decorator

--- lang/python/object/decorator.py
+++ lang/python/object/decorator.py
@@ -2,11 +2,45 @@
 
 
 
+#----------------------------------------------------------------------
+# Standard decorators
 
-class Toto:
-    init = String()
+class Person:
+    def __init__(self, name=None):
+        self.name = name
 
     @classmethod
-    def create(cls, hello):
-        return Toto
+    def create(cls, strg):
+        instance = cls(strg)
+        return instance 
+
+print Person.create('emmanuel').name
+
+#----------------------------------------------------------------------
+# Custom decorators 
+
+
+#---- Function decorators
+class myFuncDecorator(object):
+
+    def __init__(self, f):
+        print "inside myDecorator.__init__()"
+        f() # Prove that function definition has completed
+
+    def __call__(self):
+        print "inside myDecorator.__call__()"
+
+@myFuncDecorator
+def aFunction():
+    print "inside aFunction()"
+
+print "Finished decorating aFunction()"
+
+aFunction()
+
+#inside myDecorator.__init__()
+#inside aFunction()
+#Finished decorating aFunction()
+#inside myDecorator.__call__()
+
     
