acb98b92f4733f5da9467d4b9d8053cd282361bc
Emmanuel.Mayssat <emayssat@gmail.com>
2012-11-06 12:28:26 -0800

    With info on GAE

--- lang/python/object/object.py
+++ lang/python/object/object.py
@@ -9,8 +9,31 @@ class Toto(object):
     def __init__(self, two=2):
         self.two = two
 
+    @classmethod
+    def create(cls, four = 4):
+        obj = cls(four)                                 # Notice how the 'cls' is used ! Not Toto or Titi!
+        return obj
+
+print Toto.zero
+print Toto.one
+
 t = Toto()
 
 print t.zero
 print t.one
 print t.two
+
+
+class Titi(Toto):
+    three = 3
+    def __init__(self, two=2):
+        self.two = two * 2
+
+print Titi.three
+print Titi.one
+
+ti = Titi(1)
+print ti.two
+
+ti2 = Titi.create(2)
+print ti2.two
