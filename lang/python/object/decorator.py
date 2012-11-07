a3a37a70f91cda93759b1b9ced05ac00068a51e8
Emmanuel.Mayssat <emayssat@gmail.com>
2012-11-06 17:41:44 -0800

    Getting there

--- lang/python/object/decorator.py
+++ lang/python/object/decorator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-
+import time
 
 #----------------------------------------------------------------------
 # Standard decorators
@@ -25,7 +25,7 @@ class myFuncDecorator(object):
 
     def __init__(self, f):
         print "inside myDecorator.__init__()"
-        f() # Prove that function definition has completed
+        f() 
 
     def __call__(self):
         print "inside myDecorator.__call__()"
@@ -43,4 +43,38 @@ aFunction()
 #Finished decorating aFunction()
 #inside myDecorator.__call__()
 
-    
+
+#----------------------------------------------------------------------    
+# A decorator is an object whose __call__ method takes a func and return a wrapper function
+# Wherever the decorator appears, the wrapper function will be used
+
+class LInAndOut(object):
+
+    def __init__(self):
+        print "LInAndOut inside __init__"
+        self.textIn = "In:"
+        self.textOut = "Out:"
+
+    def __call__(self, func):
+        print "LInAndOut inside __call__ (%s)" % func.func_name
+        def wrapper(*args, **kwargs):
+            print "LInAndOut inside __wrapper__ (%s)" % func.func_name
+            print self.textIn
+            retVal = func(*args, **kwargs)
+            print self.textOut
+            return retVal
+        return wrapper                                           
+
+print ("========================")
+class Tata(object):
+    def __init__(self, ok=3):
+        self.ok = ok
+
+    @LInAndOut()
+    def printOk(self):
+        print self.ok
+
+print("-------------------------")
+time.sleep(1)
+it = Tata()
+it.printOk()
