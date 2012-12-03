b86c594fd95c0181caabdfb2133cd25329e0e6f3
Emmanuel.Mayssat <emayssat@gmail.com>
2012-12-03 09:13:24 -0800

    With beta peoplehelpme

---
+++ tests/mailgun/gae.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import urllib
+import urllib2
+from base64 import b64encode
+
+class User(object):
+
+    def __init__(self, name, email):
+        self.email = email  
+        self.name = name
+
+    def receive_email(self, subject="SAND No subject", text="No Text"): 
+        """ Email through MAILGUN """
+        _from = "Reciprocity Web <emayssat@gmail.com>"
+        username = "api" 
+        password = "key-6qwjwon58zad-a-pvlycilo3at50ggz1"
+        api_https_url = "https://api.mailgun.net/v2"
+        mailgun_url = api_https_url + '/reciprocity-ring.mailgun.org/messages'
+
+        data = {"from"  : _from,
+                "to"    : u"%s <%s>" % (self.name, self.email),
+                "subject": subject,
+                "text"  : text  } 
+
+        encoded_data = urllib.urlencode(data)
+        user_agent = 'python-requests/0.13.1'
+        headers = {'User-Agent' : user_agent}
+        request = urllib2.Request(mailgun_url, encoded_data, headers)
+        request.add_header('Authorization', 'Basic ' + b64encode(username + ':' + password))
+
+        r = urllib2.urlopen(request)
+        #print r.getcode()
+        #print r.headers
+        #print r.headers["content-type"]
+        return r
+
+
+u = User(name="Emmanuel Mayssat", email="emayssat@gmail.com")
+r = u.receive_email()
+
+print r.read()
+
