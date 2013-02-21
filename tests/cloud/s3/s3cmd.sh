bee02a0e3136be5af1882311702d6497f22a0e2f
Emmanuel.Mayssat <emayssat@gmail.com>
2013-02-21 13:46:46 -0800

    Getting there

--- tests/cloud/s3/s3cmd.sh
+++ tests/cloud/s3/s3cmd.sh
@@ -14,7 +14,9 @@ s3cmd get s3://bucket.lynceantech.com/s3cmd.sh /tmp/s3cmd.sh        # <-- restor
 s3cmd mv s3://bucket.lynceantech.com/s3cmd.sh s3://bucket.lynceantech.com/someother.sh # <-- rename a file
 s3cmd ls s3://bucket.lynceantech.com                                # <-- ls a bucket
 s3cmd ls                                                            # <-- list existing buckets
-s3cmd del s3://bucketname/someother.txt                             # <-- delete a file on s3
-s3cmd sync --recursive /root/ s3://bucket.lynceantech.com/root/     # <-- backup/rsync of a local directory
-s3cmd rb -r s3://bucket.lyceantech.com                              # <-- delete a bucket recursively
+s3cmd del s3://bucket.lynceantech.com/someother.sh                  # <-- delete a file on s3
+s3cmd del s3://bucket.lynceantech.com/s3cmd.sh                      # <-- delete a file on s3
+#s3cmd sync --recursive /root/ s3://bucket.lynceantech.com/root/     # <-- backup/rsync of a local directory
+s3cmd sync --recursive /root/Desktop s3://bucket.lynceantech.com/root/ # <-- backup/rsync of a local directory
+s3cmd rb -r s3://bucket.lynceantech.com                             # <-- delete a bucket recursively
 s3cmd rb s3://bucket.lynceantech.com                                # <-- delete an empty bucket (if exist)
