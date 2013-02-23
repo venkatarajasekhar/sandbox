de32332e0a763eaa47fe091081f870276c5030b2
Emmanuel.Mayssat <emayssat@gmail.com>
2013-02-22 17:26:27 -0800

    Getting there

--- tests/cloud/s3/s3access.py
+++ tests/cloud/s3/s3access.py
@@ -14,6 +14,9 @@ print myfile.read()
 # PRIVATE FILE
 
 from boto.s3.connection import S3Connection
+
+AWS_KEY = 'AKIAISKUD2QLRV4LV25A'
+AWS_SECRET = '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'
 conn = S3Connection(AWS_KEY, AWS_SECRET)
 bucket = conn.get_bucket(BUCKET)
 destination = bucket.new_key()
