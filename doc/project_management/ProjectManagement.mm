7117114200928964e38f71fde85b7f0f683e984f
Emmanuel.Mayssat <emayssat@gmail.com>
2013-06-10 16:34:38 -0700

    with project Management

---
+++ doc/project_management/ProjectManagement.mm
@@ -0,0 +1,171 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1370903887266" ID="ID_1453653472" MODIFIED="1370904079768" TEXT="Project Manager">
+<node CREATED="1370904054490" ID="ID_1176567462" MODIFIED="1370904057736" POSITION="right" TEXT="Manage">
+<node CREATED="1370903939178" FOLDED="true" ID="ID_634293095" MODIFIED="1370907184154" TEXT="Scope">
+<icon BUILTIN="full-1"/>
+<node CREATED="1370904162026" ID="ID_418816599" MODIFIED="1370904199046" TEXT="What the project is supposed to accomplish and the budget (of time and money) that has been created to achieve these objectives"/>
+<node CREATED="1370904152874" ID="ID_545427433" MODIFIED="1370904155218" TEXT="Includes">
+<node CREATED="1370904001034" ID="ID_737840194" MODIFIED="1370904005368" TEXT="Project size"/>
+<node CREATED="1370904005690" ID="ID_1011303377" MODIFIED="1370904008104" TEXT="Goals"/>
+<node CREATED="1370904008394" ID="ID_1327633227" MODIFIED="1370904011992" TEXT="Requirements"/>
+</node>
+<node CREATED="1370904042698" ID="ID_1555809862" MODIFIED="1370904248647" TEXT="">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1370904239914" ID="ID_1446588320" MODIFIED="1370904487994" TEXT="Most important part to manage. You can not effectively manage the resources, time and money in a project unless you actively manage the project scope.">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1370904371802" ID="ID_1418684730" MODIFIED="1370904375752" TEXT="Scope creep">
+<node CREATED="1370904359402" ID="ID_1325827116" MODIFIED="1370904406438" TEXT="Usually, scope changes occur in the form of &quot;scope creep&quot;. Scope creep is the piling up of small changes that by themselves are manageable, but in aggregate are significant. "/>
+<node CREATED="1370904359402" ID="ID_330752167" MODIFIED="1370904448520" TEXT="For example, the project calls for a building to be 80,000 square feet in size. The client wants to add a ten foot long, 4 foot wide awning over one bay door. That&apos;s a pretty minor change. Later the client wants to extend the awning 8 feet to cover the adjacent bay. Another minor change. Then it&apos;s a change to block the upwind side to the covered area to keep out the wind. Later, it&apos;s a request to block the other end to make the addition more symmetrical. Eventually, the client asks for a ceiling under the awning, lights in the ceiling, electrical outlets, a water faucet for the workers, some sound-proofing, and a security camera. By now, the minor change has become a major addition. Make sure any requested change, no matter how small, is accompanied by approval for a change in budget or schedule or both.">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="attach"/>
+</node>
+</node>
+<node CREATED="1370904252762" ID="ID_1768005223" MODIFIED="1370904301973" TEXT="It is absolutely imperative that any change to the scope of the project have a matching change in budget, either time or resources.">
+<node CREATED="1370904252762" ID="ID_690878486" MODIFIED="1370904321476" TEXT="If the project scope is to build a building to house three widgets with a budget of $100,000 the project manager is expected to do that. However, if the scope is changed to a building for four widgets, the project manager must obtain an appropriate change in budgeted resources. If the budget is not adjusted, the smart project manager will avoid the change in scope.">
+<icon BUILTIN="attach"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1370903935834" FOLDED="true" ID="ID_18826874" MODIFIED="1370907183434" TEXT="Money">
+<icon BUILTIN="full-2"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1370906657130" FOLDED="true" ID="ID_1090684581" MODIFIED="1370907107113" TEXT="Often a Project Manager is evaluated on his or her ability to complete a project within budget.  If you have effectively managed the project resources and project schedule, this should not be a problem.">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1370907103897" ID="ID_1033254238" MODIFIED="1370907104551" TEXT="So a project budget is composed of the estimated cost, plus the contingency and design allowance, plus any profit. The project manager&apos;s job is to keep the actual cost at or below the estimated cost, to use as little of the design allowance and contingency as possible, and to maximize the profit the company earns on the project."/>
+</node>
+<node CREATED="1370906612730" ID="ID_1780178927" MODIFIED="1370906614616" TEXT="Includes">
+<node CREATED="1370903985018" ID="ID_1424473546" MODIFIED="1370906629005" TEXT="Costs">
+<node CREATED="1370906702969" FOLDED="true" ID="ID_1974017559" MODIFIED="1370906903785" TEXT="Estimated vs Actual">
+<node CREATED="1370906897786" ID="ID_923432810" MODIFIED="1370906898632" TEXT="Each project task will have a cost, whether it is the cost of the labor hours of a computer programmer or the purchase price of a cubic yard of concrete. In preparing the project budget, each of these costs is estimated and then totaled. Some of these estimates will be more accurate than others. A company knows what it will charge each of its projects for different classifications of labor."/>
+</node>
+<node CREATED="1370906709561" FOLDED="true" ID="ID_1282323966" MODIFIED="1370907026410" TEXT="Variability">
+<node CREATED="1370906831994" ID="ID_1248242492" MODIFIED="1370906868293" TEXT=" Commodities like concrete are priced in a very competitive market so prices are fairly predictable. "/>
+<node CREATED="1370906828138" ID="ID_1032533284" MODIFIED="1370906876821" TEXT="Other estimates are less accurate. For instance, the cost of a conveyor system with higher performance specifications that normal can be estimated to be more expensive, but it is hard to determine whether it will be 10% more or 15% more. For an expensive item, that can be a significant amount."/>
+</node>
+</node>
+<node CREATED="1370903988394" ID="ID_814566549" MODIFIED="1370906726941" TEXT="Contingencies">
+<node CREATED="1370906736809" FOLDED="true" ID="ID_1241879899" MODIFIED="1370907072556" TEXT="Design allowance">
+<node CREATED="1370907005641" ID="ID_1869621159" MODIFIED="1370907006791" TEXT="When the estimated cost of an item is uncertain, the project budget often includes a design allowance. This is money that is set aside in the budget &quot;just in case&quot; the actual cost of the item is wildly different than the estimate."/>
+</node>
+<node CREATED="1370906730921" FOLDED="true" ID="ID_769561101" MODIFIED="1370907074057" TEXT="Weather, suppliers, etc">
+<node CREATED="1370907061161" ID="ID_1342838853" MODIFIED="1370907062439" TEXT="Unusual weather or problems with suppliers are always a possibility on large projects. Companies usually include a contingency amount in the project budget to cover these kinds of things."/>
+</node>
+</node>
+<node CREATED="1370903994106" ID="ID_1201885280" MODIFIED="1370906748573" TEXT="Profit">
+<node CREATED="1370906749897" ID="ID_1091074842" MODIFIED="1370906752887" TEXT="Cost"/>
+<node CREATED="1370906753161" ID="ID_1291520111" MODIFIED="1370906756248" TEXT="Contingencies"/>
+<node CREATED="1370906756505" ID="ID_6247036" MODIFIED="1370906760008" TEXT="Reminder"/>
+</node>
+</node>
+<node CREATED="1370907121641" ID="ID_1038066726" MODIFIED="1370907124087" TEXT="">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1370907129529" ID="ID_1759093052" MODIFIED="1370907158103" TEXT="To maximize your chances of meeting your project budget, meet your project schedule. The most common cause of blown budgets is blown schedules">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1370907150681" ID="ID_1108438444" MODIFIED="1370907160455" TEXT="Meeting the project schedule won&apos;t guarantee you will meet the project budget, but it significantly increases your chances.">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+</node>
+<node CREATED="1370903932202" FOLDED="true" ID="ID_1302048236" MODIFIED="1370906989355" TEXT="Time">
+<icon BUILTIN="full-3"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1370905310282" ID="ID_1548559868" MODIFIED="1370905312087" TEXT="Includes">
+<node CREATED="1370905902073" ID="ID_64327775" MODIFIED="1370905907271" TEXT="Tasks management">
+<node CREATED="1370903967978" ID="ID_1421358074" MODIFIED="1370903973640" TEXT="Task durations"/>
+<node CREATED="1370903974026" ID="ID_768496195" MODIFIED="1370905913773" TEXT="Dependencies"/>
+<node CREATED="1370906007290" ID="ID_1255419170" MODIFIED="1370906019751" TEXT="Required resources/material"/>
+</node>
+<node CREATED="1370905922026" ID="ID_1147467704" MODIFIED="1370905924615" TEXT="Schedule">
+<node CREATED="1370905934250" ID="ID_1516497902" MODIFIED="1370906170423" TEXT="Identify Tasks"/>
+<node CREATED="1370905938105" ID="ID_1895990770" MODIFIED="1370905952247" TEXT="Predecessors / Successors"/>
+<node CREATED="1370906190698" FOLDED="true" ID="ID_557788165" MODIFIED="1370906909034" TEXT="Tasks Overlapping">
+<node CREATED="1370906209546" ID="ID_1695887437" MODIFIED="1370906233095" TEXT="there are seldom enough resources and enough time to complete the tasks sequentially. Therefore, tasks have to be overlapped so several happen at the same time."/>
+</node>
+<node CREATED="1370906040522" ID="ID_851685187" MODIFIED="1370906079173" TEXT="">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1370906040522" ID="ID_783289657" MODIFIED="1370906066055" TEXT="If you omit a task, the project won&apos;t be completed. If you underestimate the length of time or the amount of resources required for the task, you may miss your schedule">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1370906084186" ID="ID_1780378541" MODIFIED="1370906098044" TEXT="The schedule can also be blown if you make a mistake in the sequencing of the tasks.">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1370906151962" ID="ID_540104887" MODIFIED="1370906155624" TEXT="For instance, think of a project called &quot;Getting Dressed In The Morning&quot;. The task &quot;put on shirt&quot; may have a longer duration if it is a buttoned dress shirt than if it&apos;s a pullover. It doesn&apos;t matter which order you complete the tasks &quot;put on right shoe&quot; and &quot;put on left shoe&quot;, but it is important to complete the &quot;put on pants&quot; task before starting the &quot;put on shoes&quot; task.">
+<icon BUILTIN="attach"/>
+</node>
+</node>
+<node CREATED="1370903977786" ID="ID_530126709" MODIFIED="1370905928061" TEXT="Critical path">
+<node CREATED="1370905966122" ID="ID_984781019" MODIFIED="1370905969895" TEXT="Float">
+<node CREATED="1370906279130" ID="ID_1149853349" MODIFIED="1370906301445" TEXT="FLOAT: When all tasks have been listed, resourced, and sequenced, you will see that some tasks have a little flexibility in their required start and finish date. "/>
+<node CREATED="1370906308154" ID="ID_453118899" MODIFIED="1370906332567" TEXT="some tasks have no flexibility, ie zero float">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1370906350730" ID="ID_29095251" MODIFIED="1370906399703" TEXT=" A line through all the tasks with zero float is called the critical path.   ">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1370906400233" ID="ID_688050216" MODIFIED="1370906416425" TEXT="The Project Manager&apos;s key time management task is to manage the critical path.">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1370905970169" ID="ID_1346898264" MODIFIED="1370906968295" TEXT="Changeable critical path">
+<node CREATED="1370906509481" ID="ID_800607548" MODIFIED="1370906524341" TEXT="Be aware, that items can be added to or removed from the critical path as circumstances change during the execution of the project. "/>
+<node CREATED="1370906497689" ID="ID_13145114" MODIFIED="1370906579879" TEXT="Installation of security cameras may not be on the critical path, but if the shipment is delayed, it may become part of the critical path. ">
+<icon BUILTIN="attach"/>
+</node>
+<node CREATED="1370906497689" ID="ID_26040145" MODIFIED="1370906579881" TEXT="Conversely, pouring the concrete foundation may be on the critical path, but if the project manager obtains an addition crew and the pour is completed early it could come off the critical path (or reduce the length of the critical path).">
+<icon BUILTIN="attach"/>
+</node>
+</node>
+<node CREATED="1370905977513" ID="ID_1347216190" MODIFIED="1370906958599" TEXT="Often multiple critical path"/>
+</node>
+</node>
+<node CREATED="1370905321114" ID="ID_693042048" MODIFIED="1370905339928" TEXT="All your skill in managing resources won&apos;t help, however, unless you can stick to the project schedule. Time management is critical in successful project management.">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1370905845194" ID="ID_521411230" MODIFIED="1370905847863" TEXT="Project Managers who succeed in meeting their project schedule have a good chance of staying within their project budget.">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1370903919723" FOLDED="true" ID="ID_187671787" MODIFIED="1370906990546" TEXT="Resources">
+<icon BUILTIN="full-4"/>
+<node CREATED="1370904558634" ID="ID_561553824" MODIFIED="1370904560903" TEXT="Includes">
+<node CREATED="1370903948026" ID="ID_602135243" MODIFIED="1370903949608" TEXT="People">
+<node CREATED="1370904806394" ID="ID_605566377" MODIFIED="1370904823336" TEXT="Project employees, vendor staff, subcontract labor"/>
+<node CREATED="1370904622943" ID="ID_1170101816" MODIFIED="1370904636741" TEXT="This includes the labor hours of the designers, the builders, the testers and the inspectors on the project team."/>
+<node CREATED="1370905086074" ID="ID_1415777151" MODIFIED="1370905168677" TEXT="Usually there is a team lead (who knows more about the details than you do and manage the workers directly). The project manager provides the direction">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1370903949882" ID="ID_639801479" MODIFIED="1370903952472" TEXT="Equipment">
+<node CREATED="1370905195850" ID="ID_293370073" MODIFIED="1370905202031" TEXT="You have to make sure you have the right equipment in the right place at the right time and that it has the supplies it needs to operate properly."/>
+<node CREATED="1370904829722" ID="ID_840693908" MODIFIED="1370904861800" TEXT="Cranes, trucks, backhoes, other heavy equipment or  Development, test, and staging servers, CD burners or  Recording studio, tape decks, mixers, microphones and speakers">
+<icon BUILTIN="attach"/>
+</node>
+<node CREATED="1370905221162" ID="ID_607503932" MODIFIED="1370905243655" TEXT="The equipment you have to manage as part of your project depends on the nature of the project">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1370903952746" ID="ID_775483874" MODIFIED="1370903965144" TEXT="Material">
+<node CREATED="1370905274314" ID="ID_1613329351" MODIFIED="1370905276799" TEXT="The project management issue with supplies is to make sure the right supplies arrive at the right time (we&apos;ll talk about the right price later)."/>
+<node CREATED="1370904845546" ID="ID_1625208336" MODIFIED="1370904858440" TEXT="Concrete, pipe, rebar, insulation or  CD blanks, computers, jewel cases, instruction manuals">
+<icon BUILTIN="attach"/>
+</node>
+</node>
+<node CREATED="1370904871370" ID="ID_1979820913" MODIFIED="1370904874664" TEXT="Productivity">
+<node CREATED="1370904930618" ID="ID_1684931548" MODIFIED="1370904946391" TEXT="Know what need to be done"/>
+<node CREATED="1370904946810" ID="ID_1969370988" MODIFIED="1370904956504" TEXT="Incentive to do it correctly"/>
+<node CREATED="1370904974746" ID="ID_664506533" MODIFIED="1370905031688" TEXT="people, equipment, material coordination">
+<node CREATED="1370904993802" ID="ID_962523899" MODIFIED="1370905006296" TEXT="Managing the people resources means having the right people, with the right skills and the proper tools, in the right quantity at the right time"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
