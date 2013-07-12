dd13fe4bac94f6135787d391f2f4b7cc539e6bad
Emmanuel.Mayssat <emayssat@gmail.com>
2013-07-12 12:57:23 -0700

    Getting there with bash

--- system/linux/command/bash/bash.sh
+++ system/linux/command/bash/bash.sh
@@ -25,4 +25,22 @@ echo "${ARR[1]}"
 #   HOMSUM=`eval du -s ~$who`
 #done
 
+for i in `seq 3 5 `; do                 # with external command
+  echo $i
+done
+
+echo "Two by Two"
+step=2
+#for i in {0..10..$step}; do                 # builtin
+for i in {0..10..2}; do                 # builtin
+  echo $i
+done
+
+echo "C style syntax"
+step=3
+for ((c=1; c<=15;c=c+$step))            # Supported by /bin/bash and not /bni/sh !!!
+do
+    echo $c
+done
+
 
